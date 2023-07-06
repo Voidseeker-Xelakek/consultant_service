cf2d6dfe9d8eebbb0facc6d55a94a4737ec7fa18
Mitrich89 <sazonkin.v@yandex.ru>
2023-07-06 21:38:55 +0300

    second

---
+++ front/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>Анализатор рисков при покупке недвижимости</span>
+            <ul className='nav'>
+                <li>О нас</li>
+                <li>Контакты</li>
+                <li>Кабинет</li>
+            </ul>
+        </div>
+        <div className='presentation'></div>
+    </header> 
+  )
+}
