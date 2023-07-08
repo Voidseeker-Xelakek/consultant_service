816b9b897cc79330524f2de9641d77602ce0a540
Mitrich89 <sazonkin.v@yandex.ru>
2023-07-08 12:51:36 +0300

    добавил показ доп рекомендаций

---
+++ front/src/components/Addition.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Addition({ Addition }) {
+  return (
+    <div className="default-container">
+      <p>{Addition}</p>
+    </div>
+  );
+}
--- front/src/components/Recomendations.js
+++ front/src/components/Recomendations.js
@@ -1,18 +1,17 @@
 import React from "react";
+import ToggleVisibility from "./ToggleVisibility";
+import Addition from "./Addition";
 
 export default function Recomendations({ Recomendations }) {
-  console.log(Recomendations);
-  function handleClick() {
-    // let a = (document.getElementById("p").value = "longText");
-    // document.getElementById("p").innerHTML = a;
-  }
   return (
     <div className="recomendation">
       <h1>Рекомендации</h1>
       {Recomendations.map((el, i) => (
         <div key={i} className="shortRecomandation">
           {el.shortText}
-          <button onClick={handleClick}>Дополнение</button>
+          <ToggleVisibility>
+            <Addition Addition={el.longText} />
+          </ToggleVisibility>
         </div>
       ))}
     </div>
