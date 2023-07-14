3694adef96c9b299631f8cae79b52ba534246e5d
Arseniy1123 <arseniytushin@mail.ru>
2023-07-14 18:36:35 +0300

    Ссылки на сайт + рек по порядку

--- front/src/components/Recomendations.js
+++ front/src/components/Recomendations.js
@@ -4,24 +4,63 @@ import Addition from "./Addition";
 
 export default function Recomendations({ recomendations, indexes }) {
   console.log(indexes);
+  let recommendationNumber = 1;
+
   return (
     <div className="recomendation">
       <h1>Рекомендации</h1>
-      {recomendations.map((el, i) => (
-        <div key={i} className="shortRecomandation">
-          {indexes.includes(el.id) && (
+      {recomendations.map((el, i) => {
+        if (!indexes.includes(el.id)) {
+          return null;
+        }
+
+        const formattedShortText = el.shortText && replaceLinks(el.shortText);
+        const formattedLongText = el.longText && replaceLinks(el.longText);
+
+        const formattedShortTextWithLink = el.shortText && (
+          <span
+            dangerouslySetInnerHTML={{ __html: formattedShortText }}
+          ></span>
+        );
+
+        const formattedLongTextWithLink = el.longText && (
+          <span
+            dangerouslySetInnerHTML={{ __html: formattedLongText }}
+          ></span>
+        );
+
+        return (
+          <div key={i} className="shortRecomandation">
             <div className="recommendation-item">
-              <span className="recommendation-id">{el.id}.</span>{" "}
-              <span className="recommendation-text">{el.shortText}</span>
+              <span className="recommendation-id">
+                {recommendationNumber++}.
+              </span>{" "}
+              <span className="recommendation-text">
+                {formattedShortTextWithLink || el.shortText}
+              </span>
               {el.longText !== null && (
                 <AdditionVisibility>
-                  <Addition addition={el.longText} />
+                  {formattedLongTextWithLink && (
+                    <Addition addition={formattedLongTextWithLink} />
+                  )}
                 </AdditionVisibility>
               )}
             </div>
-          )}
-        </div>
-      ))}
+          </div>
+        );
+      })}
     </div>
   );
 }
+
+function replaceLinks(text) {
+  return text.replace(
+    /https?:\/\/[^\s)]+/g,
+    (match) =>
+      `<a href="${match}" target="_blank" rel="noopener noreferrer">${match}</a>`
+  );
+}
+
+
+
+
