2faac32da1e9d5cd11f54139d58316b34be19645
Voidseeker-Xelakek <kekeev.alesha@yandex.ru>
2023-07-15 15:44:33 +0300

    Увеличил название, симметрично кабинет и калькулятор, код цвет закомменчен

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -8,10 +8,10 @@ export default function Header() {
     <header className="App-header">
       <div className="left-section"><img src={domPNG} alt="DOM" style={{width: '100px', height: '100px'}} /></div>
       <div className="center-section">
-        <span className="name">Недвижимость без риска</span>
+        <span className="name" style={{ fontSize: '50px' }}>Недвижимость без риска</span>
       </div>
-      <div className="right-section">
-        <li onClick={openCalculatorWindow}>Калькулятор</li>
+      <div className="right-section" style={{ display: 'flex', alignItems: 'center' }}>
+        <li onClick={openCalculatorWindow} style={{ marginRight: '10px' }}>Калькулятор</li>
         <li>Кабинет</li>
       </div>
     </header>
