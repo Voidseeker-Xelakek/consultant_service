013e78780924d200b6f0ff56510ed50c70bde6ac
Mitrich89 <sazonkin.v@yandex.ru>
2023-07-26 16:01:46 +0300

    little upgrade

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 export default function Footer() {
+  const relevanceDate =
+    new Date().toLocaleString("en-US", {
+      day: "2-digit",
+    }) +
+    "." +
+    new Date().toLocaleString("en-US", {
+      month: "2-digit",
+    }) +
+    "." +
+    new Date().getFullYear();
   return (
     <footer>
-      <div>Информация, размещенная на сайте, носит справочный характер</div>
+      <div>Актуально на {relevanceDate}</div>
+      <div>Информация, размещенная на сайте, носит справочный характер.</div>
       <div>Все права защищены &copy;</div>
     </footer>
   );
